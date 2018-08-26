5e6c045e2b35152d40e8a06a0513e601d88f32ba
sirrah23 <hseth93@gmail.com>
2018-08-26 13:13:08 -0400

    Create a basic registration page

--- tfmanager/app/views.py
+++ tfmanager/app/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, 'app/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
