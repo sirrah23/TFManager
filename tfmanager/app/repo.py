0d291d8e894f657709a78ba01eb1368ba8c834d8
sirrah23 <hseth93@gmail.com>
2018-08-26 22:18:12 -0400

    Can create a folder via a folder repository

---
+++ tfmanager/app/repo.py
@@ -0,0 +1,42 @@
+from app.models import Folder
+from django.contrib.auth.models import User
+
+
+class FolderRepo:
+
+    @staticmethod
+    def to_json(folder_obj):
+        res = {}
+        res['id'] = folder_obj.id
+        res['name'] = folder_obj.name
+        res['deleted'] = folder_obj.deleted
+        res['owner_id'] = folder_obj.owner.id
+        res['creation_time'] = folder_obj.creation_time
+        res['parent_id'] = folder_obj.parent.id if folder_obj.parent else None
+        return res
+
+    @staticmethod
+    def get_all_folder_for_user(user_id):
+        pass
+
+    @staticmethod
+    def get_all_folder_for_user_with_parent(user_id, parent_id):
+        pass
+
+    @staticmethod
+    def create_folder(user_id, name, parent_id=None):
+        user = User.objects.filter(id=user_id).first()
+        parent = None
+        if not user:
+            return None
+        if parent_id:
+            parent = Folder.objects.filter(id=parent_id).first()
+            if not parent:
+                return None
+        folder = Folder(name=name, owner=user, parent=parent)
+        folder.save()
+        return FolderRepo.to_json(folder)
+
+    @staticmethod
+    def delete_folder(user_id, folder_id):
+        pass
--- tfmanager/app/tests.py
+++ tfmanager/app/tests.py
@@ -1,5 +1,6 @@
 from django.test import Client, TestCase
 from django.contrib.auth.models import User
+from app.repo import FolderRepo
 
 
 class RegistrationTest(TestCase):
@@ -34,3 +35,48 @@ class LoginTest(TestCase):
             '/app/login/', {'username': 'baduser', 'password': 'badpass'})
         # No redirect to main page of the app
         self.assertEqual(response.status_code, 200)
+
+
+class FolderRepoTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        response = self.client.post(
+            '/app/register/', {'username': 'nu001', 'password1': '@pass1212', 'password2': '@pass1212'})
+        self.user = User.objects.all().first()
+
+    def test_create_folder_no_parent_one(self):
+        f = FolderRepo.create_folder(self.user.id, 'folder1')
+        self.assertIsNotNone(f)
+        self.assertEqual('id' in f, True)
+        self.assertEqual('creation_time' in f, True)
+        self.assertEqual(f['name'], 'folder1')
+        self.assertEqual(f['deleted'], False)
+        self.assertEqual(f['owner_id'], self.user.id)
+        self.assertEqual(f['parent_id'], None)
+
+    def test_create_folder_no_parent_two(self):
+        f = FolderRepo.create_folder(self.user.id, 'folder2')
+        self.assertIsNotNone(f)
+        self.assertEqual('id' in f, True)
+        self.assertEqual('creation_time' in f, True)
+        self.assertEqual(f['name'], 'folder2')
+        self.assertEqual(f['deleted'], False)
+        self.assertEqual(f['owner_id'], self.user.id)
+        self.assertEqual(f['parent_id'], None)
+
+    def test_create_folder_with_parent(self):
+        f1 = FolderRepo.create_folder(self.user.id, 'folder1')
+        self.assertIsNotNone(f1)
+        f2 = FolderRepo.create_folder(
+            self.user.id, 'folder2', parent_id=f1['id'])
+        self.assertIsNotNone(f2)
+
+        self.assertEqual(f1['name'], 'folder1')
+        self.assertEqual(f2['name'], 'folder2')
+        self.assertEqual(f1['deleted'], False)
+        self.assertEqual(f2['deleted'], False)
+        self.assertEqual(f1['owner_id'], self.user.id)
+        self.assertEqual(f2['owner_id'], self.user.id)
+        self.assertEqual(f1['parent_id'], None)
+        self.assertEqual(f2['parent_id'], f1['id'])
