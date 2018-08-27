c23bd2cf409909f037bf01b2f4d5fef4d5394306
sirrah23 <hseth93@gmail.com>
2018-08-26 21:35:52 -0400

    Create models for File and Folder

--- tfmanager/app/models.py
+++ tfmanager/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Folder(models.Model):
+    name = models.CharField(max_length=50)
+    deleted = models.BooleanField(default=False)
+    creation_time = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='owner')
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, related_name='parent_folder', blank=True, null=True)
+
+
+class File(models.Model):
+    name = models.CharField(max_length=50)
+    deleted = models.BooleanField(default=False)
+    creation_time = models.DateTimeField(auto_now=True)
+    belong = models.ForeignKey(
+        Folder, on_delete=models.CASCADE, related_name='belong_folder', blank=True, null=True)
